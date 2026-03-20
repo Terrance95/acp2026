1c78bc6125aac65f74410d7dccc0151e7f17cd3a
Terrance95 <ugcet2502196@reva.edu.in>
2026-03-20 21:52:42 +0530

    demo8 first commit

---
+++ level1/demo8.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+// Function Prototypes
+void input(char *str);
+void concatenate_strings(char *str1, char *str2);
+void display(char *str);
+
+int main() {
+
+    char str1[100], str2[100];
+
+    input(str1);
+    input(str2);
+
+    concatenate_strings(str1, str2);
+
+    display(str1);
+
+    return 0;
+}
+
+void input(char *str) {
+    printf("Enter string:\n");
+    fgets(str,100,stdin);
+    // Write code to read the string
+}
+
+void concatenate_strings(char *str1, char *str2) {
+    // Write code to concatenate str2 to str1 using while/do while
+    int i=0,j=0;
+    while(str2[i]!='\0'){
+        i++;
+    }
+    while(str1[j]!='\0'){
+        str2[i]=str1[j];
+        i++;
+        j++;
+    }
+}
+
+void display(char *str) {
+    printf("Concatenated string:%s\n",str);
+    // Write code to display the string
+}

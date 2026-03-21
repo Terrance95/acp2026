85eb11856ebb97f3a69f5028bd14d048450cf51d
Terrance95 <ugcet2502196@reva.edu.in>
2026-03-21 08:27:02 +0530

    act1

--- level1/act1.c
+++ level1/act1.c
@@ -1,66 +1,60 @@
 #include <stdio.h>
 
-void inputTemperatures(int n, float temps[n]);
-float findHighest(int n, float temps[n]);
-float findLowest(int n, float temps[n]);
+void inputTemperatures(int n, float temps[]);
+float findHighest(int n, float temps[]);
+float findLowest(int n, float temps[]);
 void output(float max, float min);
 
 int main() {
     int n;
     float max, min;
+
     printf("enter no of temperatures u want to enter:");
-    scanf("%d",&n);
-    if(n<=0){
-        printf("invalid value for n entered:");
+    if (scanf("%d", &n) != 1 || n <= 0) {
+        printf("invalid value for n entered\n");
         return 0;
     }
+
     float temps[n];
-    // Call function to input 7 temperatures
-    inputTemperatures(n,temps);
-    // Call function to find highest temperature
-    max=findHighest(n,temps);
-    // Call function to find lowest temperature
-    min=findHighest(n,temps);
-    // Call function to display results
-    output(max,min);
+
+    inputTemperatures(n, temps);
+
+    max = findHighest(n, temps);
+    min = findLowest(n, temps); // Fixed: previously calling findHighest
+
+    output(max, min);
 
     return 0;
 }
 
-void inputTemperatures(int n, float temps[n]) {
+void inputTemperatures(int n, float temps[]) {
     printf("Enter temperatures:\n");
-    for(int i=0;i<n;i++){
-        scanf("%f",&temps[i]);
+    for (int i = 0; i < n; i++) {
+        scanf("%f", &temps[i]);
     }
-    // Write code to read temperatures
 }
 
-float findHighest(int n, float temps[n]) {
-    // Write code to find highest temperature
-    float max=0;
-    for(int i=0;i<n;i++){
-        if(temps[i]>max){
-            max=temps[i];
+float findHighest(int n, float temps[]) {
+    float max = temps[0]; // Start with the first element
+    for (int i = 1; i < n; i++) {
+        if (temps[i] > max) {
+            max = temps[i];
         }
     }
     return max;
-
 }
 
-float findLowest(int n, float temps[n]) {
-    // Write code to find lowest temperature
-    float min=temps[0];
-    for(int i=0;i<n;i++){
-        if(temps[i]<min){
-            min=temps[i];
+float findLowest(int n, float temps[]) {
+    float min = temps[0]; // Start with the first element
+    for (int i = 1; i < n; i++) {
+        if (temps[i] < min) {
+            min = temps[i];
         }
     }
     return min;
-
 }
 
 void output(float max, float min) {
     printf("Highest temperature: %.2f\n", max);
     printf("Lowest temperature: %.2f\n", min);
-    // Write code to display results
 }

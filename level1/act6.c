b0605d91bfbfaf67c63ac169d77cb69b7f9cbe09
Terrance95 <ugcet2502196@reva.edu.in>
2026-03-30 21:41:58 +0530

    1st commit

--- level1/act6.c
+++ level1/act6.c
@@ -1,46 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 
-void read_string(char str[], int size);
-void print_string(char str[]);
-void swap_strings(char str1[], char str2[]);
+void read_string(char *str, int size);
+void swap_strings(char *str1, char *str2, int size);
 
 int main() {
-
     char str1[100], str2[100];
 
-    printf("Enter first string:\n");
+    printf("Enter first string: ");
     read_string(str1, 100);
 
-    printf("Enter second string:\n");
+    printf("Enter second string: ");
     read_string(str2, 100);
 
-    swap_strings(str1, str2);
+    swap_strings(str1, str2, 100);
 
-    printf("After swapping:\n");
-
-    printf("First string: ");
-    print_string(str1);
-
-    printf("Second string: ");
-    print_string(str2);
+    printf("\n--- After Optimized Swap ---\n");
+    printf("First: %s", str1);
+    printf("Second: %s", str2);
 
     return 0;
 }
 
-void read_string(char str[], int size) {
-    scanf("%s", str);
+void read_string(char *str, int size) {
+    if (fgets(str, size, stdin)) {
+        str[strcspn(str, "\n")] = 0;
+    }
 }
 
-void print_string(char str[]) {
-    printf("%s\n", str);
-}
-
-void swap_strings(char str1[], char str2[]) {
-
-    char temp[100];
-
-    strcpy(temp, str1);
-    strcpy(str1, str2);
-    strcpy(str2, temp);
+void swap_strings(char *str1, char *str2, int size) {
+    char temp[size]; 
+    memcpy(temp, str1, size);
+    memcpy(str1, str2, size);
+    memcpy(str2, temp, size);
 }
